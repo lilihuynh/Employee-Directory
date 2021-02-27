aaa0c2884f0e420cadc8bd7a5355726a84ccdbda
Lili Huynh <lillianhuynh312@gmail.com>
2021-02-26 18:26:20 -0800

    initiate REACT APP

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Wrapper from "./components/Wrapper"
+import Main from "./components/Main";
+function App() {
+  return (
+    <Wrapper>
+     <Main/>
+    </Wrapper>
+  );
+}
+
+export default App;
